4e170bf53adeb3176ec9ca1cfafeb6676d50694c
Joon <pss21242124@gmail.com>
2025-02-23 23:00:08 +0900

    테일윈드 추가, 엘리먼트플러스 및 아이콘 추가,프리텐다드 폰트 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,23 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// 엘리먼트 플러스 아이콘 임포트
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+// 엘리먼트 플러스 아이콘 등록
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.use(ElementPlus);
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
