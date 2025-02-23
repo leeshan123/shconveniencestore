3fb1d4ad40b6f69d90aea65ca310fda15b1456f3
Joon <pss21242124@gmail.com>
2025-02-24 00:12:47 +0900

    로그인 페이지 구조 및 css수정

--- frontend/src/views/user/Signin.vue
+++ frontend/src/views/user/Signin.vue
@@ -1,38 +1,53 @@
 <template>
-  <div>
-    <div>
-      <div>로고</div>
-      <div>
-        <div>아이디 입력폼</div>
-        <div>비밀번호 입력폼</div>
-        <div>
-          <div>체크박스</div>
-          <div>아이디저장</div>
-          <div>로그인 버튼</div>
+  <div class="flex justify-center py-20">
+    <div class="flex flex-col items-center">
+      <div class="mb-10">
+        <img width="130px" src="@/assets/icon/logo.svg" alt="승환편의점로고" />
+      </div>
+      <div class="flex flex-col items-center">
+        <el-input
+          v-model="input"
+          style="width: 240px"
+          placeholder="아이디"
+          class="mb-3"
+        />
+        <el-input
+          v-model="input"
+          style="width: 240px"
+          placeholder="비밀번호"
+          class="mb-6"
+        />
+        <div class="flex items-center justify-between w-full mb-8">
+          <el-checkbox v-model="checked1" label="아이디 저장" size="large" />
+          <el-button color="#4285F4" type="primary">로그인</el-button>
         </div>
         <div>
-          <ul>
-            <li>회원가입</li>
-            <li>아아디찾기</li>
+          <ul class="flex gap-1">
+            <li>회원가입 /</li>
+            <li>아이디찾기 /</li>
             <li>비밀번호 찾기</li>
           </ul>
         </div>
       </div>
     </div>
   </div>
-  <div>
-    <label>
-      <input type="checkbox" v-model="isChecked" />
-      체크박스 선택
-    </label>
-    <p>체크 상태: {{ isChecked }}</p>
-  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+const input = ref('');
 
-const isChecked = ref(false); // 기본값 false
+const checked1 = ref(true);
 </script>
 
-<style scoped></style>
+<style scoped>
+/* 체크된 상태의 체크박스 색상 변경 */
+::v-deep(.el-checkbox__input.is-checked .el-checkbox__inner) {
+  background-color: #4285f4; /* 체크된 상태의 배경색 */
+}
+
+/* 체크된 상태의 라벨 색상 */
+::v-deep(.el-checkbox__input.is-checked + .el-checkbox__label) {
+  color: #4285f4; /* 체크되었을 때 라벨 색상 변경 */
+}
+</style>
